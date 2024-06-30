da7f5b237e121e5b43be3db2a726b5e9654339d1
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2024-06-30 18:20:24 +0200

    Meilenstein: Ein Punkt in der Mitte, der sich zum Magneten bewegt oder gar nicht.

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 let sprite = game.createSprite(2, 2)
+let Magnetgrenzwert = 400
 basic.forever(function () {
     basic.pause(500)
-    if (Math.abs(input.magneticForce(Dimension.X)) > 500 || Math.abs(input.magneticForce(Dimension.Y)) > 500) {
+    if (Math.abs(input.magneticForce(Dimension.X)) > Magnetgrenzwert || Math.abs(input.magneticForce(Dimension.Y)) > Magnetgrenzwert) {
         if (Math.abs(input.magneticForce(Dimension.X)) > Math.abs(input.magneticForce(Dimension.Y))) {
             if (input.magneticForce(Dimension.X) > 0) {
                 sprite.change(LedSpriteProperty.X, 1)
@@ -10,9 +11,9 @@ basic.forever(function () {
             }
         } else {
             if (input.magneticForce(Dimension.Y) > 0) {
-                sprite.change(LedSpriteProperty.Y, -1)
-            } else {
                 sprite.change(LedSpriteProperty.Y, 1)
+            } else {
+                sprite.change(LedSpriteProperty.Y, -1)
             }
         }
     } else {
