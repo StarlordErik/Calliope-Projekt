ad85eb60217db5140d5479ea18d1cac5223c522a
StarlordErik <163322364+StarlordErik@users.noreply.github.com>
2024-06-30 22:58:19 +0200

    Meilenstein: Alle möglichen Blöcke-Blöcke in Funktionen zusammengefasst

--- main.ts
+++ main.ts
@@ -1,83 +1,111 @@
+function schattenFolgtSnake () {
+    basic.pause(100)
+    Schatten.set(LedSpriteProperty.X, SnakeMini.get(LedSpriteProperty.X))
+    Schatten.set(LedSpriteProperty.Y, SnakeMini.get(LedSpriteProperty.Y))
+}
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    game.setScore(0)
-    Snackanzahl = 0
-    SnakeMini = game.createSprite(2, 2)
-    Schatten = game.createSprite(2, 2)
-    Snack = game.createSprite(randint(0, 4), randint(0, 4))
-    Richtung = randint(0, 3)
-    for (let index = 0; index < 60; index++) {
-        if (input.buttonIsPressed(Button.B)) {
-            break;
-        }
-        if (SnakeMini.get(LedSpriteProperty.X) == Snack.get(LedSpriteProperty.X) && SnakeMini.get(LedSpriteProperty.Y) == Snack.get(LedSpriteProperty.Y)) {
-            Snack.set(LedSpriteProperty.X, randint(0, 4))
-            Snack.set(LedSpriteProperty.Y, randint(0, 4))
-            Snackanzahl += 1
-        }
+    initialisiereSpiel()
+    while (übrigeSpieldauer > 0) {
+        brichDasSpielAbWenn(input.buttonIsPressed(Button.B))
+        SnakeFrisstWenn(SnakeMini.get(LedSpriteProperty.X) == Snack.get(LedSpriteProperty.X) && SnakeMini.get(LedSpriteProperty.Y) == Snack.get(LedSpriteProperty.Y))
+        snakeBewegtSich()
         basic.pause(400)
-        if (Math.abs(input.magneticForce(Dimension.X)) > Magnetgrenzwert || Math.abs(input.magneticForce(Dimension.Y)) > Magnetgrenzwert) {
-            if (Math.abs(input.magneticForce(Dimension.X)) > Math.abs(input.magneticForce(Dimension.Y))) {
-                if (input.magneticForce(Dimension.X) > 0) {
-                    SnakeMini.change(LedSpriteProperty.X, 1)
-                } else {
-                    SnakeMini.change(LedSpriteProperty.X, -1)
-                }
-            } else {
-                if (input.magneticForce(Dimension.Y) > 0) {
-                    SnakeMini.change(LedSpriteProperty.Y, 1)
-                } else {
-                    SnakeMini.change(LedSpriteProperty.Y, -1)
-                }
-            }
-            xDifferenz = SnakeMini.get(LedSpriteProperty.X) - Schatten.get(LedSpriteProperty.X)
-            yDifferenz = SnakeMini.get(LedSpriteProperty.Y) - Schatten.get(LedSpriteProperty.Y)
-            if (yDifferenz == 1) {
-                Richtung = 0
-            } else if (xDifferenz == 1) {
-                Richtung = 1
-            } else if (yDifferenz == -1) {
-                Richtung = 2
-            } else {
-                Richtung = 3
-            }
-            basic.pause(100)
-            Schatten.set(LedSpriteProperty.X, SnakeMini.get(LedSpriteProperty.X))
-            Schatten.set(LedSpriteProperty.Y, SnakeMini.get(LedSpriteProperty.Y))
+        übrigeSpieldauer += -0.5
+    }
+    beendeSpiel()
+})
+function SnakeFrisstWenn (snakeIstAufSnack: boolean) {
+    if (snakeIstAufSnack) {
+        Snack.set(LedSpriteProperty.X, randint(0, 4))
+        Snack.set(LedSpriteProperty.Y, randint(0, 4))
+        Punkte += 1
+    }
+}
+function snakeWirdVomMagnetenBewegt () {
+    if (Math.abs(input.magneticForce(Dimension.X)) > Math.abs(input.magneticForce(Dimension.Y))) {
+        if (input.magneticForce(Dimension.X) > 0) {
+            SnakeMini.change(LedSpriteProperty.X, 1)
         } else {
-            if (Richtung == 0) {
-                SnakeMini.change(LedSpriteProperty.Y, 1)
-            } else if (Richtung == 1) {
-                SnakeMini.change(LedSpriteProperty.X, 1)
-            } else if (Richtung == 2) {
-                SnakeMini.change(LedSpriteProperty.Y, -1)
-            } else {
-                SnakeMini.change(LedSpriteProperty.X, -1)
-            }
-            basic.pause(100)
-            Schatten.set(LedSpriteProperty.X, SnakeMini.get(LedSpriteProperty.X))
-            Schatten.set(LedSpriteProperty.Y, SnakeMini.get(LedSpriteProperty.Y))
+            SnakeMini.change(LedSpriteProperty.X, -1)
+        }
+    } else {
+        if (input.magneticForce(Dimension.Y) > 0) {
+            SnakeMini.change(LedSpriteProperty.Y, 1)
+        } else {
+            SnakeMini.change(LedSpriteProperty.Y, -1)
         }
     }
+}
+function beendeSpiel () {
     SnakeMini.delete()
     Schatten.delete()
     Snack.delete()
     game.addScore(1)
     basic.pause(500)
     for (let index = 0; index < 3; index++) {
-        if (input.buttonIsPressed(Button.B)) {
-            break;
-        }
-        basic.showNumber(Snackanzahl)
+        basic.showNumber(Punkte)
     }
     game.addScore(1)
-})
+}
+function snakeMerktSichDieRichtung () {
+    xDifferenz = SnakeMini.get(LedSpriteProperty.X) - Schatten.get(LedSpriteProperty.X)
+    yDifferenz = SnakeMini.get(LedSpriteProperty.Y) - Schatten.get(LedSpriteProperty.Y)
+    if (yDifferenz == 1) {
+        Richtung = 0
+    } else if (xDifferenz == 1) {
+        Richtung = 1
+    } else if (yDifferenz == -1) {
+        Richtung = 2
+    } else {
+        Richtung = 3
+    }
+}
+function snakeBewegtSichInDieGemerkteRichtung () {
+    if (Richtung == 0) {
+        SnakeMini.change(LedSpriteProperty.Y, 1)
+    } else if (Richtung == 1) {
+        SnakeMini.change(LedSpriteProperty.X, 1)
+    } else if (Richtung == 2) {
+        SnakeMini.change(LedSpriteProperty.Y, -1)
+    } else {
+        SnakeMini.change(LedSpriteProperty.X, -1)
+    }
+}
+function snakeBewegtSich () {
+    if (Math.abs(input.magneticForce(Dimension.X)) > Magnetgrenzwert || Math.abs(input.magneticForce(Dimension.Y)) > Magnetgrenzwert) {
+        snakeWirdVomMagnetenBewegt()
+        snakeMerktSichDieRichtung()
+        schattenFolgtSnake()
+    } else {
+        snakeBewegtSichInDieGemerkteRichtung()
+        schattenFolgtSnake()
+    }
+}
+function brichDasSpielAbWenn (knopfGedrückt: boolean) {
+    if (knopfGedrückt) {
+        übrigeSpieldauer += übrigeSpieldauer * -1
+    }
+}
+function initialisiereSpiel () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    game.setScore(0)
+    Punkte = 0
+    SnakeMini = game.createSprite(2, 2)
+    Schatten = game.createSprite(2, 2)
+    Snack = game.createSprite(randint(0, 4), randint(0, 4))
+    Richtung = randint(0, 3)
+    übrigeSpieldauer = 30
+}
+let Richtung = 0
 let yDifferenz = 0
 let xDifferenz = 0
-let Richtung = 0
+let Punkte = 0
 let Snack: game.LedSprite = null
-let Schatten: game.LedSprite = null
+let übrigeSpieldauer = 0
 let SnakeMini: game.LedSprite = null
-let Snackanzahl = 0
+let Schatten: game.LedSprite = null
 let Magnetgrenzwert = 0
 Magnetgrenzwert = 400
 let Richtungsmapping = [
